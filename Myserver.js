07c909b1304303b7940e791afa98c8176dd126f4
Ran <laron.ran@gmail.com>
2017-01-20 22:47:41 -0800

    Initial commit

---
+++ Myserver.js
@@ -0,0 +1,14 @@
+var http = require ("http");
+
+function process_reuqest(req, res) {
+    var body = "Hi Ran\n"
+    var content_length = body.length;
+    res.writeHead(200,{
+        'content-Length' : content_length,
+        'content_Type' : 'text/plain'
+    });
+    res.end(body);
+}
+
+var s = http.createServer(process_reuqest);
+s.listen(8080);
