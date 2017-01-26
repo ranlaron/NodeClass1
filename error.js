533c7b205a79a4f27410bacea26e375ad9b25439
Ran <laron.ran@gmail.com>
2017-01-26 13:52:40 -0800

    Update error.js

--- error.js
+++ error.js
@@ -2,7 +2,7 @@
 
 setTimeout(function () {
     try {
-        throw new Error("DANGER ZONE!");
+        throw new Error("DANGER ZONE! ran");
     } catch (e) {
         console.log("I caught the error PHEW!");
     }
